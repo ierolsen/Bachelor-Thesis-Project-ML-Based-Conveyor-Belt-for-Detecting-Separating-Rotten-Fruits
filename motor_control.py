0a19636a742c766099633cf84b375ec4799335b4
Erol <ierolsen@outlook.com>
2023-01-26 14:54:19 +0300

    add motor control realtime database on firebase

---
+++ motor_control.py
@@ -0,0 +1,24 @@
+from pyrebase import pyrebase
+from firebase_secrets import SECRETS
+
+firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
+db = firebase.database()
+db.child("motor-control")
+
+data = {
+    "CONTROL":{
+        "detected": 0,
+        "num_rotten": 0,
+        "num_fresh": 0,
+        "open": False,
+        "close": True,
+    }
+}
+
+db.set(data)
+
+#FOR UPDATING THE DATA
+#db.update(data)
+
+#FOR DELETING A VALUE
+# e.g. : db.child("CONTROL").child("DETECTED").remove()
