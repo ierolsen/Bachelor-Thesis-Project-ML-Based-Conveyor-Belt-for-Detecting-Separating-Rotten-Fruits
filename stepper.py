d77331d3c089c48ccaa12d7c7738d2a75f3223d6
Erol <ierolsen@outlook.com>
2023-01-26 16:36:43 +0300

    update stepper motor

--- stepper.py
+++ stepper.py
@@ -2,8 +2,8 @@ from time import sleep
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
-import parameters_base # containes motor parameters
-import control_parameters # contains the values for opening and closing the stepper motor
+import motor_parameters_reader as mpr # containes motor parameters
+import motor_control_reader as mcr # contains the values for opening and closing the stepper motor
 
 #define motor1
 GPIO_pins = (14, 15, 18) # Microstep Resolution MS1-MS3 -> GPIO Pin
@@ -13,16 +13,16 @@ step = 21      # Step -> GPIO Pin
 # Declare an named instance of class pass GPIO pins numbers
 motor1 = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
 
-motor1.motor_go(clockwise=parameters_base.cw_clockwise,
-                steptype=parameters_base.cw_steptype,
-                steps=parameters_base.cw_steps,
-                stepdelay=parameters_base.cw_stepdelay,
-                verbose=parameters_base.cw_verbose,
-                initdelay=parameters_base.cw_initdelay)
+motor1.motor_go(clockwise=mpr.cw_clockwise,
+                steptype=mpr.cw_steptype,
+                steps=mpr.cw_steps,
+                stepdelay=mpr.cw_stepdelay,
+                verbose=mpr.cw_verbose,
+                initdelay=mpr.cw_initdelay)
 
-motor1.motor_go(clockwise=parameters_base.ccw_clockwise,
-                steptype=parameters_base.ccw_steptype,
-                steps=parameters_base.ccw_steps,
-                stepdelay=parameters_base.ccw_stepdelay,
-                verbose=parameters_base.ccw_verbose,
-                initdelay=parameters_base.ccw_initdelay)
+motor1.motor_go(clockwise=mpr.ccw_clockwise,
+                steptype=mpr.ccw_steptype,
+                steps=mpr.ccw_steps,
+                stepdelay=mpr.ccw_stepdelay,
+                verbose=mpr.ccw_verbose,
+                initdelay=mpr.ccw_initdelay)
