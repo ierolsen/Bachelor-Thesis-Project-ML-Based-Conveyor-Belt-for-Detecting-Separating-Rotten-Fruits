67e863b3c7c63b7b7a8ab6b0b98ea7b1249a1baa
Erol <ierolsen@outlook.com>
2023-01-26 15:11:40 +0300

    update name

---
+++ motor_control_config.py
@@ -0,0 +1,22 @@
+from pyrebase import pyrebase
+from firebase_secrets import SECRETS
+
+firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
+db = firebase.database()
+
+"""
+    detected": 0,
+    "num_rotten": 0,
+    "num_fresh": 0,
+    "open": False,
+    "close": True,
+"""
+
+detected = db.child("motor-control").child("CONTROL").child("detected").get().val()
+num_rotten = db.child("motor-control").child("CONTROL").child("num_rotten").get().val()
+num_fresh = db.child("motor-control").child("CONTROL").child("num_fresh").get().val()
+open = db.child("motor-control").child("CONTROL").child("open").get().val()
+close = db.child("motor-control").child("CONTROL").child("close").get().val()
+
+#FOR UPDATING THE DATA
+#db.update(data)
