1e3ae81823723040c2d5262d1499ccd0fcb8584a
Erol <ierolsen@outlook.com>
2023-01-28 00:49:14 +0300

    update the model, there is an error that must be fixed!!

--- model/xception.py
+++ model/xception.py
@@ -1,44 +1,43 @@
-from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, GlobalAveragePooling2D, Activation, Flatten, Dropout, BatchNormalization
+from keras import optimizers, regularizers
 from keras.applications.xception import Xception
-from keras import Model, layers
-from keras.losses import CategoricalCrossentropy
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.applications.xception import preprocess_input
+from keras.models import Model
+from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.metrics import AUC
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
 from pathlib import Path
 import numpy as np
-import cv2
 
 class XceptionModel(object):
     
     def __init__(self, config):
         self.config = config
-        self.model = model
-        self.hist = hist
+        self.model = None
+        self.hist = None
         
     def create_model(self):
         size = self.config.get("size")
-        number_of_class = self.config.get("number_of_class")
         
-        base_model = Xception(include_top=False, weights="imagenet", input_shape=(size, size, 3))
-        x = layers.Flatten()(base_model.output)
-        x = layers.Dense(1024)(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Dense(512)(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Dense(128)(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Dense(64)(x)
-        x = layers.BatchNormalization()(x)
-        predictions = layers.Dense(number_of_class, activation="softmax")(x)
-        
-        model = Model(inputs=base_model.input, outputs=[predictions])
-        
-        loss = CategoricalCrossentropy(label_smoothing=self.config.get("label_smoothing"))
-        
-        model.compile(loss=loss, 
-                      optimizer=Adam(lr=self.config.get("lr")), 
-                      metrics=["accuracy"])
+        model = Sequential()
+        model.add(Xception(include_top=False, pooling='avg', weights="imagenet", input_shape=(size, size, 3)))
+        model.add(Dense(1024))
+        model.add(Dropout(0.4))
+        model.add(Dense(512))
+        model.add(Dropout(0.4))
+        model.add(Dense(256))
+        model.add(Dropout(0.4))
+        model.add(Dense(128))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(self.config.get("number_of_class"), activation='softmax'))
+        model.layers[0].trainable = False
+       
+        model.compile(optimizer=Adam(lr=self.config.get("lr")), 
+                      loss='categorical_crossentropy', 
+                      metrics=['accuracy'])
         
         self.model = model
         
@@ -46,65 +45,67 @@ class XceptionModel(object):
     def train_model(self):
         Path(self.config.get("save_path")).parent.mkdir(parents=True, exist_ok=True)
         
+        """
         reduce_lr = ReduceLROnPlateau(monitor='val_loss',
                                       factor=0.1,
                                       patience=2,
                                       min_lr=1e-14,
-                                      verbose=1)
-
+                                      verbose=0)
+        
         early_stop = EarlyStopping(monitor='val_loss', 
                                    patience=self.config.get("stopping_patience"), 
                                    verbose=1,
                                    restore_best_weights=True)
-
+        """
         checkpoint = ModelCheckpoint(filepath=self.config.get("save_path"), 
                                      monitor='val_loss', mode='min', 
                                      save_weights_only=True, 
                                      save_best_only=True, 
                                      verbose=1)
+        
               
         batch = self.config.get("batch")
+        size = self.config.get("size")
         
-        datagen = ImageDataGenerator(rescale=1./255,
-                                     shear_range=0.2,
-                                     zoom_range=0.2,
+        datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                     rescale=1./255.,
+                                     shear_range=0.2,zoom_range=0.2, 
                                      horizontal_flip=True,
                                      rotation_range=45,
                                      validation_split=0.3)
         
-        size = self.config.get("size")
         train_datagen = datagen.flow_from_directory(directory=self.config.get("train_path"),
-                            target_size = (size,size),
-                            color_mode='rgb',
-                            batch_size=batch,
-                            class_mode = 'sparse',
-                            subset='training')
+                                                    target_size = (size,size),
+                                                    color_mode='rgb',
+                                                    batch_size=batch,
+                                                    class_mode = 'sparse',
+                                                    shuffle=True,
+                                                    subset='training')
+
+        valid_datagen = datagen.flow_from_directory(directory=self.config.get("train_path"),
+                                                    target_size = (size,size),
+                                                    color_mode='rgb',
+                                                    batch_size=batch,
+                                                    class_mode='sparse',
+                                                    shuffle=True,
+                                                    subset='validation')
         
-        valid_datagen = datagen.flow_from_directory(directory=self.config.get("test_path"),
-                                                   target_size = (size,size),
-                                                   color_mode='rgb',
-                                                   batch_size=batch,
-                                                   class_mode='sparse',
-                                                   subset='validation')
-        
-        hist = self.model.fit_generator(train_datagen,
-                                        steps_per_epoch=train_datagen.n//batch,
+        test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                        rescale=1./255.)
+
+        test_datagen = test_datagen.flow_from_directory(directory=self.config.get("test_path"),
+                                                        target_size = (size,size),
+                                                        color_mode='rgb',
+                                                        batch_size=batch,
+                                                        class_mode='sparse')
+
+        hist = self.model.fit_generator(generator=train_datagen,
+                                        steps_per_epoch=train_datagen.n // batch,
                                         epochs=self.config.get("epochs"),
                                         validation_data=valid_datagen,
-                                        validation_steps=valid_datagen.n//batch,
-                                        verbose=1,
-                                        callbacks=[reduce_lr, early_stop, checkpoint])
+                                        validation_steps=valid_datagen.n // batch,
+                                        verbose=0, callbacks=[checkpoint])
 
         self.hist = hist
-
-    def predict(self, img):
         
-        datagen = ImageDataGenerator(rescale=1./255,
-                                     shear_range=0.2,
-                                     zoom_range=0.2,
-                                     horizontal_flip=True,
-                                     rotation_range=45)
-        
-        img = self.datagen.flow(img)
-        prediction = self.model.predict(img[None, :, :, :])[0]
-        return prediction
+        print("the end..")
