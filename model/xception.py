771ee258f165f1b8b74d1d2a6d4e081685d24367
Erol <ierolsen@outlook.com>
2023-01-29 04:33:06 +0300

    change parameters

--- model/xception.py
+++ model/xception.py
@@ -1,75 +1,70 @@
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, GlobalAveragePooling2D, Activation, Flatten, Dropout, BatchNormalization
-from keras import optimizers, regularizers
 from keras.applications.xception import Xception
 from keras.applications.xception import preprocess_input
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-
 from pathlib import Path
 import numpy as np
 
+
 class XceptionModel(object):
-    
+
     def __init__(self, config):
         self.config = config
         self.model = None
         self.hist = None
-        
+
     def create_model(self):
         size = self.config.get("size")
-        
+ 
         model = Sequential()
         model.add(Xception(include_top=False, pooling='avg', weights="imagenet", input_shape=(size, size, 3)))
-        model.add(Dense(1024))
-        model.add(Dropout(0.4))
         model.add(Dense(512))
         model.add(Dropout(0.4))
         model.add(Dense(128))
         model.add(Dropout(0.4))
         model.add(Dense(64))
-        model.add(Activation('relu'))
         model.add(Dropout(0.4))
         model.add(Dense(self.config.get("number_of_class"), activation='softmax'))
         model.layers[0].trainable=False
-       
+
         model.compile(optimizer=Adam(lr=self.config.get("lr")), 
                       loss='sparse_categorical_crossentropy', 
                       metrics=['accuracy'])
-        
+
         self.model = model
-        
-    
+
     def train_model(self):
         Path(self.config.get("save_path")).parent.mkdir(parents=True, exist_ok=True)
-        
-        
+
         reduce_lr = ReduceLROnPlateau(monitor='val_loss',
                                       factor=0.1,
                                       patience=2,
                                       min_lr=1e-14,
                                       verbose=1)
-        
+
         early_stop = EarlyStopping(monitor='val_loss', 
                                    patience=self.config.get("stopping_patience"), 
                                    verbose=1,
                                    restore_best_weights=True)
-        
+
         checkpoint = ModelCheckpoint(filepath=self.config.get("save_path"), 
-                                     monitor='val_loss', mode='min', 
+                                     monitor='val_loss', 
+                                     mode='min', 
                                      save_weights_only=True, 
                                      save_best_only=True, 
                                      verbose=1)
-        
-              
+
         batch = self.config.get("batch")
         size = self.config.get("size")
-        
+
         datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
                                      rescale=1./255.,
-                                     shear_range=0.2,zoom_range=0.2, 
+                                     shear_range=0.2,
+                                     zoom_range=0.2, 
                                      horizontal_flip=True,
                                      rotation_range=45,
                                      validation_split=0.3)
@@ -89,7 +84,7 @@ class XceptionModel(object):
                                                     class_mode='sparse',
                                                     shuffle=True,
                                                     subset='validation')
-        
+
         test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
                                           rescale=1./255.)
 
@@ -108,5 +103,3 @@ class XceptionModel(object):
                                         callbacks=[checkpoint, early_stop, reduce_lr])
 
         self.hist = hist
-        
-        print("DONE")
