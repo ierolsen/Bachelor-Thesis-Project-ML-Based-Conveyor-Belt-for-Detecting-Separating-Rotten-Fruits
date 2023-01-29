8f486184d530f758bd847f5c19d37a9039495b03
Erol <ierolsen@outlook.com>
2023-01-29 20:20:27 +0300

    delete a useless parameter

--- motor_control_db.py
+++ motor_control_db.py
@@ -11,14 +11,14 @@ data = {
         "num_rotten": 0,
         "num_fresh": 0,
         "open": False,
-        "close": True,
     }
 }
 
+# RUN IT ONCE, IT CREATES TABLE ON FIREBASE
 db.set(data)
 
 #FOR UPDATING THE DATA
-db.update(data)
+#db.update(data)
 
 #FOR DELETING A VALUE
 # e.g. : db.child("CONTROL").child("DETECTED").remove()
