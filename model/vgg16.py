b7b94fb0cae20c90692f951ae2ebdc80efb212b6
Erol <ierolsen@outlook.com>
2023-02-01 21:14:14 +0300

    add vgg16 model

---
+++ model/vgg16.py
@@ -0,0 +1,103 @@
+import keras
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.applications import VGG16
+from keras.applications.imagenet_utils import preprocess_input
+from keras.losses import BinaryCrossentropy
+from keras.metrics import BinaryAccuracy
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from pathlib import Path
+
+
+class VGG16(object):
+    
+    def __init__(self, config):
+        self.config = config
+        self.model = None
+        self.hist = None
+        
+    def create_model(self):
+        size = self.config.get("size")
+        
+        base_model = keras.applications.VGG16(weights='imagenet', input_shape=(size, size, 3), include_top=False);
+        
+        x = GlobalAveragePooling2D()(base_model.output)
+        predictions = Dense(self.config.get("number_of_class"), activation=self.config.get("activation"))(x)
+        
+        model = Model(inputs=base_model.input, outputs=[predictions])
+        
+        model.compile(optimizer=keras.optimizers.Adam(),
+                      loss=self.config.get("loss"), 
+                      metrics=[BinaryAccuracy()])
+        
+        self.model = model
+        
+    def train_model(self):
+        Path(self.config.get("save_path")).parent.mkdir(parents=True, exist_ok=True)
+
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                                      factor=0.1,
+                                      patience=3,
+                                      min_lr=1e-14,
+                                      verbose=1)
+
+        early_stop = EarlyStopping(monitor='val_loss', 
+                                   patience=self.config.get("stopping_patience"), 
+                                   verbose=1,
+                                   restore_best_weights=True)
+
+        checkpoint = ModelCheckpoint(filepath=self.config.get("save_path"), 
+                                     monitor='val_loss', 
+                                     mode='min', 
+                                     save_weights_only=True, 
+                                     save_best_only=True, 
+                                     verbose=1)
+
+        batch = self.config.get("batch")
+        size = self.config.get("size")
+
+        datagen = ImageDataGenerator(samplewise_center=True,
+                                     rotation_range=10,
+                                     zoom_range=0.1,
+                                     width_shift_range=0.1,
+                                     height_shift_range=0.1,
+                                     horizontal_flip=True,
+                                     vertical_flip=True,
+                                     validation_split=0.3)
+        
+        train_datagen = datagen.flow_from_directory(directory=self.config.get("train_path"),
+                                                    target_size=(size,size),
+                                                    color_mode='rgb',
+                                                    batch_size=batch,
+                                                    class_mode="binary",
+                                                    shuffle=True,
+                                                    subset='training')
+
+        valid_datagen = datagen.flow_from_directory(directory=self.config.get("train_path"),
+                                                    target_size=(size,size),
+                                                    color_mode='rgb',
+                                                    batch_size=batch,
+                                                    class_mode="binary",
+                                                    shuffle=True,
+                                                    subset='validation')
+
+        test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                          rescale=1./255.)
+
+        test_datagen = test_datagen.flow_from_directory(directory=self.config.get("test_path"),
+                                                        target_size=(size,size),
+                                                        color_mode='rgb',
+                                                        batch_size=batch,
+                                                        class_mode='sparse')
+
+        hist = self.model.fit_generator(generator=train_datagen,
+                                        steps_per_epoch=train_datagen.n // batch,
+                                        epochs=self.config.get("epochs"),
+                                        validation_data=valid_datagen,
+                                        validation_steps=valid_datagen.n // batch,
+                                        verbose=1, 
+                                        callbacks=[checkpoint, early_stop, reduce_lr])
+
+        self.hist = hist
+        
