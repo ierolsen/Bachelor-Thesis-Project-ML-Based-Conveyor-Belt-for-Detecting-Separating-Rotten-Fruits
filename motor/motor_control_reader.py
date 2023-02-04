1d2fb0bd4c02db5ad429b7821171dbd314bc8379
Erol <ierolsen@outlook.com>
2023-02-04 12:51:34 +0300

    move files into motor folder #4

---
+++ motor/motor_control_reader.py
@@ -0,0 +1,18 @@
+from pyrebase import pyrebase
+from firebase_secrets import SECRETS
+
+firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
+db = firebase.database()
+
+"""
+    detected": 0,
+    "num_rotten": 0,
+    "num_fresh": 0,
+    "open": False,
+"""
+
+detected = db.child("motor-control").child("CONTROL").child("detected").get().val()
+num_rotten = db.child("motor-control").child("CONTROL").child("num_rotten").get().val()
+num_fresh = db.child("motor-control").child("CONTROL").child("num_fresh").get().val()
+opening = db.child("motor-control").child("CONTROL").child("open").get().val()
+closing = db.child("motor-control").child("CONTROL").child("close").get().val()
