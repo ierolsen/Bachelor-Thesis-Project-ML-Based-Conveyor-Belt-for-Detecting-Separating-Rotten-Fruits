6a1c312c083dab24d1fa1401f23d6bd200db0021
Erol <ierolsen@outlook.com>
2023-02-07 23:40:08 +0300

    there is an error must be solved

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import cv2
 
-from pyrebase import pyrebase
-from firebase_secrets import SECRETS
+#from pyrebase import pyrebase
+#from firebase_secrets import SECRETS
 
-firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
-db = firebase.database()
+import keras
+import tensorflow as tf
+
+#firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
+#db = firebase.database()
 
 """
 data = {"open": True,
@@ -19,14 +22,58 @@ result = db.child("motor-control").child("CONTROL").update(data)
 print(result)
 """
 
-#model = Xception(weights="model/model.h5")
-#model.summary()
-
-#model = Model.load_weights(filepath="model/model.hdf5")
-#model = load_model(filepath="model/model.h5")
-#model.summary()
-
 """
+def binary_accuracy(y_true, y_pred):
+    return keras.metrics.binary_accuracy(y_true, tf.round(y_pred))
+
+dependencies = {
+    'binary_accuracy': binary_accuracy,
+    'config': {'name': binary_accuracy, 
+               'dtype':'float32', 
+               'threshold': 0.5}
+}
+
+model = keras.models.load_model("model/model_060223.h5", custom_objects=dependencies)
+"""
+
+import tensorflow as tf
+
+class BinaryAccuracy(tf.keras.metrics.Metric):
+    def __init__(self, name='binary_accuracy', dtype=tf.float32, threshold=0.5, **kwargs):
+        super(BinaryAccuracy, self).__init__(name=name, dtype=dtype, **kwargs)
+        self.threshold = threshold
+        self.accuracy = self.add_weight(name='accuracy', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.where(y_pred > self.threshold, 1, 0)
+        values = tf.math.equal(y_true, y_pred)
+        values = tf.cast(values, self.dtype)
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, self.dtype)
+            sample_weight = tf.convert_to_tensor(sample_weight)
+            values = tf.multiply(values, sample_weight)
+        self.accuracy.assign_add(tf.reduce_mean(values))
+
+    def result(self):
+        return self.accuracy
+
+    def get_config(self):
+        config = {
+            'threshold': self.threshold,
+            'dtype': str(self.dtype),
+        }
+        base_config = super(BinaryAccuracy, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+dependencies = {
+    'BinaryAccuracy': BinaryAccuracy
+}
+
+model = keras.models.load_model("model/model_060223.h5", custom_objects=dependencies)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
+
+#model.summary()
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -51,4 +98,3 @@ while True:
         
 cap.release()
 cv2.destroyAllWindows()
-"""
