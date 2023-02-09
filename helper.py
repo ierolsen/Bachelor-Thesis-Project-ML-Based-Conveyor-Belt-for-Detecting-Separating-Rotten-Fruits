e7ab114ecdaeb58ae4b8a869d726777c99dffbdf
Erol <ierolsen@outlook.com>
2023-02-09 14:21:01 +0300

    add helper functions

---
+++ helper.py
@@ -0,0 +1,28 @@
+from pyrebase import pyrebase
+from motor.firebase_secrets import SECRETS
+
+
+# Initialize FireBase DB.
+firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
+db = firebase.database()
+
+
+def run_motor(db):
+    """
+    FireBase DataBase updater. It will run when a rotten fruit is detected. 
+    It will cause DC-Motor run.
+    Args:
+        db: FireBase DataBase
+    """
+    data = {"run": True}
+    db.child("motor-control").child("CONTROL").update(data)    
+
+def stop_motor(db):
+    """
+    FireBase DataBase updater.
+    It will stop to DC-Motor run.
+    Args:
+        db: FireBase DataBase
+    """
+    data = {"run": False}
+    db.child("motor-control").child("CONTROL").update(data)
