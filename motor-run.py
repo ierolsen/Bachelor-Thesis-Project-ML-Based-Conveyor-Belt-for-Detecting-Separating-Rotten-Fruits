dd365f49739de3583a5b8ddcd1bdf5fdecabbd07
Erol <ierolsen@outlook.com>
2023-02-12 13:18:10 +0300

    fix getting value from firebase error

--- motor-run.py
+++ motor-run.py
@@ -6,18 +6,19 @@ from firebase_secrets import SECRETS
 firebase = pyrebase.initialize_app(SECRETS["FIREBASE"])
 db = firebase.database()
 
-# Get value of run (True of False) from FiraBase
-run = db.child("motor-control").child("CONTROL").child("run").get().val()
-
-# Define the DC-motor
+# Define motor
 motor = Motor(Ena=2, In1=3, In2=4)
 
 while True:
     
+    # Get value of run (True of False)
+    run = db.child("motor-control").child("CONTROL").child("run").get().val()
+    
     if run == True:
-        print(run)
         motor.move_backward(speed=50, t=0.60)
         motor.stop()
         motor.move_forward(speed=50, t=0.48)
         motor.stop()
-        break
+        
+    else:
+        continue
