e1b68f583b5f93c2d9044136041abc3576a84abf
Erol <ierolsen@outlook.com>
2023-02-12 13:54:49 +0300

    feat: add new function for drawing borderline around the belt

--- helper.py
+++ helper.py
@@ -33,23 +33,47 @@ def stop_motor(db):
 def count_rottens(db):
     pass
 
+def draw_borderline(frame, pt1, pt2, pt3, pt4, color=(0, 255, 0), thickness=2):
+    """
+    pt1-------pt2
+    |         |
+    |         |
+    |         |
+    |         |
+    |         |
+    pt3-------pt4
+    (150, 0) -> pt1
+    (466, 0) -> pt2
+    (0, 480) -> pt3
+    (640, 480) -> pt4
+    """
+    cv2.line(frame, pt1, pt2, color, thickness)
+    cv2.polylines(frame, [np.array([[pt1[0], pt1[1]], 
+                                    [pt3[0], pt3[1]]])], True, color, thickness)
+        
+    cv2.polylines(frame, [np.array([[pt2[0], pt2[1]], 
+                                    [pt4[0], pt4[1]]])], True, color, thickness)
+    cv2.line(frame, pt3, pt4, color, thickness)
+
 def getMouseCoordinates(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         # Print the x,y coordinates of the point clicked
         print(f"Coordinates: ({x}, {y})")
 
-rect_points = np.array([[(145, 10), (145, 105), (472, 105), (472, 10)]])
 
-"""
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow('frame')
-cv2.setMouseCallback('frame', getMouseCoordinates)
+
+# Click desired points and get coordinates
+#cv2.setMouseCallback('frame', getMouseCoordinates) 
 
 
 while True:
 
     ret, frame = cap.read()
+ 
+    draw_borderline(frame, pt1=(150, 0), pt2=(466, 0), pt3=(0, 480), pt4=(640, 480))
     
     cv2.imshow('frame', frame)
     
@@ -61,4 +85,3 @@ while True:
 # Release the camera and destroy the windows
 cap.release()
 cv2.destroyAllWindows()
-"""
